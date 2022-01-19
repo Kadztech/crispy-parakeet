2c57c48ac1633427c5ce0105a5058b040c33f0ff
chomba <chomba@bongohive.co.zm>
2022-01-19 20:57:26 +0200

    update image portal section

--- about.php
+++ about.php
@@ -1,79 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8" />
-        <title>About us - Kadztech software services</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
-        <meta name="keywords" content="bootstrap 4, premium, marketing, multipurpose" />
-        <meta content="Shreethemes" name="author" />
-        <!-- favicon -->
-        <link rel="shortcut icon" href="images/favicon.ico">
-        <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <!-- Magnific Popup -->
-        <link href="css/magnific-popup.css" rel="stylesheet" type="text/css" />
-        <!-- Icons -->
-        <link href="css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
-        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
-        <!-- Css -->
-        <link href="css/style.css" rel="stylesheet" type="text/css" id="theme-opt" />
-        <link href="css/colors/default.css" rel="stylesheet" id="color-opt">
+<head>
+    <meta charset="utf-8" />
+    <title>About us - Kadztech software services</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Premium Bootstrap 4 Landing Page Template" />
+    <meta name="keywords" content="bootstrap 4, premium, marketing, multipurpose" />
+    <meta content="Shreethemes" name="author" />
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Magnific Popup -->
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <!-- Icons -->
+    <link href="css/materialdesignicons.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+    <!-- Css -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" id="theme-opt" />
+    <link href="css/colors/default.css" rel="stylesheet" id="color-opt">
 
-    </head>
+</head>
 
-    <body>
-        
-        <!-- navigation -->
-        <?php include "./php/components/Nav.php"?>
+<body>
 
-        <!-- Home Start -->
-        <section style="padding-bottom: 24px;" class="bg-half-170 d-table w-100 bg-light" style="background: url('images/bg/page.png') top center;" id="home">
-            <div class="container">
-                <div class="row mt-5">
-                    <div class="col-lg-12">
-                        <div class="title-heading text-center text-md-left">
-                            <nav aria-label="breadcrumb" class="d-inline-block mt-4">
-                                <ul class="breadcrumb mb-0">
-                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">About</li>
-                                </ul>
-                            </nav>
-                            <h3>About Us</h3>
-                            <p class="text-muted para-desc mt-3 mb-0">
-                                We exist to transform your business for growth and impact through digital services and accurate analytics.
-                            </p> 
-                            <p class="text-muted para-desc mt-3 mb-0">
-                                We measure our success by the long-lasting impact we deliver for our clients. This philosophy has helped us build a strong reputation for adding business value.
-                            </p> 
-                            
-                        </div>
-                    </div><!--end col-->
-                </div><!--end row-->
-            </div> <!--end container-->
-        </section><!--end section-->
-        <div class="position-relative">
-            <div class="shape overflow-hidden text-white">
-                <svg viewBox="0 0 1000 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M 0 0 c 0 0 200 50 500 50 s 500 -50 500 -50 v 101 h -1000 v -100 z" fill="currentColor"></path>
-                </svg>
-            </div>
-        </div>
-        <!-- Home End -->
+    <!-- navigation -->
+    <?php include "./php/components/Nav.php"?>
 
-        <div class="container mt-100 mt-60">
-            <div class="row justify-content-center">
-                <div class="col-12">
-                    <div class="section-title text-center mb-4 pb-2">
-                        <h4 class="title mb-4">Team Members</h4>
-                        <p class="text-muted mx-auto para-desc mb-0">Create, collaborate, and turn your ideas into incredible products with the definitive platform for digital design.</p>
+    <!-- Home Start -->
+    <section style="padding-bottom: 24px;" class="bg-half-170 d-table w-100 bg-light"
+        style="background: url('images/bg/page.png') top center;" id="home">
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-lg-12">
+                    <div class="title-heading text-center text-md-left">
+                        <nav aria-label="breadcrumb" class="d-inline-block mt-4">
+                            <ul class="breadcrumb mb-0">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">About</li>
+                            </ul>
+                        </nav>
+                        <h3>About Us</h3>
+                        <p class="text-muted para-desc mt-3 mb-0">
+                            We exist to transform your business for growth and impact through digital services and
+                            accurate analytics.
+                        </p>
+                        <p class="text-muted para-desc mt-3 mb-0">
+                            We measure our success by the long-lasting impact we deliver for our clients. This
+                            philosophy has helped us build a strong reputation for adding business value.
+                        </p>
+
                     </div>
-                </div><!--end col-->
-            </div><!--end row-->
+                </div>
+                <!--end col-->
+            </div>
+            <!--end row-->
+        </div>
+        <!--end container-->
+    </section>
+    <!--end section-->
+    <div class="position-relative">
+        <div class="shape overflow-hidden text-white">
+            <svg viewBox="0 0 1000 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M 0 0 c 0 0 200 50 500 50 s 500 -50 500 -50 v 101 h -1000 v -100 z" fill="currentColor"></path>
+            </svg>
+        </div>
+    </div>
+    <!-- Home End -->
 
-            <div class="row" style="justify-content: center;">
-                <!-- <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
+    <div class="container mt-100 mt-60">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="section-title text-center mb-4 pb-2">
+                    <h4 class="title mb-4">Team Members</h4>
+                    <p class="text-muted mx-auto para-desc mb-0">Create, collaborate, and turn your ideas into
+                        incredible products with the definitive platform for digital design.</p>
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+
+        <div class="row" style="justify-content: center;">
+            <!-- <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
                     <div class="card border-0 team overflow-hidden">
                         <img src="images/client/01.jpg" class="img-fluid rounded-pill" alt="">
                         <div class="card-body content pt-4 text-center">
@@ -83,7 +93,7 @@
                     </div>
                 </div> -->
 
-                <!-- <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
+            <!-- <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
                     <div class="card border-0 team overflow-hidden">
                         <img src="images/client/02.jpg" class="img-fluid rounded-pill" alt="">
                         <div class="card-body content pt-4 text-center">
@@ -93,31 +103,35 @@
                     </div>
                 </div> -->
 
-                <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
-                    <div class="card border-0 team overflow-hidden">
-                        <img src="images/client/03.jpg" class="img-fluid rounded-pill" alt="">
-                        <div class="card-body content pt-4 text-center">
-                            <h5 class="mb-0">Miyanda Miyanda</h5>
-                            <small class="text-muted">Founder / C.E.O</small>
-                        </div>
+            <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
+                <div class="card border-0 team overflow-hidden">
+                    <img src="images/client/03.jpeg" class="img-fluid rounded-pill" alt="">
+                    <div class="card-body content pt-4 text-center">
+                        <h5 class="mb-0">Miyanda Miyanda</h5>
+                        <small class="text-muted">Founder / C.E.O</small>
                     </div>
-                </div><!--end col-->
+                </div>
+            </div>
+            <!--end col-->
 
-                <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
-                    <div class="card border-0 team overflow-hidden">
-                        <img src="images/client/04.jpg" class="img-fluid rounded-pill" alt="">
-                        <div class="card-body content pt-4 text-center">
-                            <h5 class="mb-0">Chomba Chanda</h5>
-                            <small class="text-muted">Founder / C.T.O</small>
-                        </div>
+            <div class="col-lg-3 col-md-6 col-12 mt-4 pt-2">
+                <div class="card border-0 team overflow-hidden">
+                    <img src="images/client/04.jpg" class="img-fluid rounded-pill" alt="">
+                    <div class="card-body content pt-4 text-center">
+                        <h5 class="mb-0">Chomba Chanda</h5>
+                        <small class="text-muted">Founder / C.T.O</small>
                     </div>
-                </div><!--end col-->
-            </div><!--end row-->
-        </div><!--end container-->
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+        <!--end row-->
+    </div>
+    <!--end container-->
 
 
-   
-        <!-- <section class="section">
+
+    <!-- <section class="section">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-6 col-md-6 col-12">
@@ -140,49 +154,54 @@
                 </div>
             </div> -->
 
-            
 
 
-            
-           
 
-                <div class="row mt-5 pt-4 justify-content-center" style="margin-bottom: 100px;">
-                    <div class="col-12 text-center">
-                        <div class="section-title">
-                            <h4 class="title mb-3">Have Question ? Get in touch!</h4>
-                            <p class="text-muted para-desc mx-auto">Create, collaborate, and turn your ideas into incredible products with the definitive platform for digital design.</p>
-                            <a href="./contact.php" class="btn btn-primary mt-4"><i class="mdi mdi-phone"></i> Contact us</a>
-                        </div>
-                    </div><!--end col-->
-                </div><!--end row-->
-            </div><!--end container-->
-        </section><!--end section-->
-        
-        <!-- End -->
 
-        <?php include "./php/components/Footer.php" ?>
-        <!-- Footer End -->
 
-        
 
-        <!-- Back to top -->
-        <a href="#" class="btn btn-icon btn-soft-primary back-to-top"><i data-feather="arrow-up" class="icons"></i></a>
-        <!-- Back to top -->
-        
-        <!-- javascript -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/jquery.easing.min.js"></script>
-        <script src="js/scrollspy.min.js"></script>
-        <!-- Magnific Popup -->
-        <script src="js/jquery.magnific-popup.min.js"></script>
-        <script src="js/magnific.init.js"></script>
-        <!-- Icons -->
-        <script src="js/feather.min.js"></script>
-        
-        <!-- Main Js -->
-        <script src="js/app.js"></script>
-        
-    </body>
+    <div class="row mt-5 pt-4 justify-content-center" style="margin-bottom: 100px;">
+        <div class="col-12 text-center">
+            <div class="section-title">
+                <h4 class="title mb-3">Have Question ? Get in touch!</h4>
+                <p class="text-muted para-desc mx-auto">Create, collaborate, and turn your ideas into incredible
+                    products with the definitive platform for digital design.</p>
+                <a href="./contact.php" class="btn btn-primary mt-4"><i class="mdi mdi-phone"></i> Contact us</a>
+            </div>
+        </div>
+        <!--end col-->
+    </div>
+    <!--end row-->
+    </div>
+    <!--end container-->
+    </section>
+    <!--end section-->
+
+    <!-- End -->
+
+    <?php include "./php/components/Footer.php" ?>
+    <!-- Footer End -->
+
+
+
+    <!-- Back to top -->
+    <a href="#" class="btn btn-icon btn-soft-primary back-to-top"><i data-feather="arrow-up" class="icons"></i></a>
+    <!-- Back to top -->
+
+    <!-- javascript -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrollspy.min.js"></script>
+    <!-- Magnific Popup -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific.init.js"></script>
+    <!-- Icons -->
+    <script src="js/feather.min.js"></script>
+
+    <!-- Main Js -->
+    <script src="js/app.js"></script>
+
+</body>
 
 </html>
